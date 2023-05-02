558a98cd9efea2f9cd0981d97d4ec550daa4b0ce
Madhusha Fonseka <madhushafonseka@gmail.com>
2023-05-02 20:00:55 +0530

    Auth change

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -17,11 +17,11 @@ function authJwt(){
         path: [
            /* // {url: `${api}/products` , methods: ['GET', 'OPTIONS']}, */
 
-            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS']},
-            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS']},
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS']},
-            `${api}/users/login`,            
-            `${api}/users/register`
+            // {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS']},
+            // {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS']},
+            // {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS']},
+            // `${api}/users/login`,            
+            // `${api}/users/register`
 
             
             // { url: /(.*)/}
